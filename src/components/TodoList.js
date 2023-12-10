478000cc3a89f86e83192ecd795b124af61e8bc5
Saai Vignesh P <saaivigneshpremanand@gmail.com>
2023-12-10 10:41:02 +0530

    implement drag-n-drop feature

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,22 +3,25 @@ import { nanoid } from "nanoid";
 import TodoListItem from "./TodoListItem";
 import "./TodoList.css";
 
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+
 const TodoList = ({ todoList }) => {
-  const [list, setList] = useState([
-    ...JSON.parse(localStorage.getItem("todoList") || "[]"),
-  ]);
+  const [list, setList] = useState([...JSON.parse(localStorage.getItem("todoList") || "[]")]);
 
   useEffect(() => {
     localStorage.setItem("todoList", JSON.stringify(list));
   }, [list]);
 
   const updateListItem = (item) => {
-    const existingItemIndex = list.findIndex(
-      (existingItem) => existingItem.id === item.id
-    );
+    const existingItemIndex = list.findIndex((existingItem) => existingItem.id === item.id);
     if (existingItemIndex >= 0) {
+      if (item.done && list[existingItemIndex] !== item.done) {
+        list.splice(existingItemIndex, 1);
+        list.push(item);
+      } else {
+        list[existingItemIndex] = item;
+      }
       const updatedList = [...list];
-      updatedList[existingItemIndex] = item;
       setList(updatedList);
     }
   };
@@ -27,15 +30,13 @@ const TodoList = ({ todoList }) => {
     const newItem = {
       id: nanoid(),
       done: false,
-      description: "",
+      description: ""
     };
     setList([...list, newItem]);
   };
 
   const deleteListItem = (item) => {
-    const existingItemIndex = list.findIndex(
-      (existingItem) => existingItem.id === item.id
-    );
+    const existingItemIndex = list.findIndex((existingItem) => existingItem.id === item.id);
     if (existingItemIndex >= 0) {
       list.splice(existingItemIndex, 1);
       const updatedList = [...list];
@@ -43,31 +44,53 @@ const TodoList = ({ todoList }) => {
     }
   };
 
+  const onDragEnd = (result) => {
+    if (!result.destination) return;
+    const { source, destination } = result;
+    const updatedList = [...list];
+    const [movedItem] = updatedList.splice(source.index, 1);
+    updatedList.splice(destination.index, 0, movedItem);
+    setList(updatedList);
+  };
+
   return (
     <div className="todo-list-container">
       <h3>To-Do List</h3>
-      <div className="todo-item-container">
-        {list.length ? (
-          list.map((item) => {
-            return (
-              <TodoListItem
-                key={item.id}
-                item={item}
-                updateListItem={updateListItem}
-                deleteListItem={deleteListItem}
-              />
-            );
-          })
-        ) : (
-          <div className="todo-empty">
-            <h2>Hooray!</h2>
-            <p>Nothing left to complete.</p>
-            <p>
-              To add a task click on <b>Add New Item</b>
-            </p>
-          </div>
-        )}
-      </div>
+
+      <DragDropContext onDragEnd={(result) => onDragEnd(result)}>
+        <Droppable key="mainColumn" droppableId="mainColumn">
+          {(provided, snapshot) => (
+            <div
+              className={`todo-item-container ${snapshot.isDraggingOver ? "disable-pointer" : ""}`}
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              {list.length ? (
+                list.map((item, index) => {
+                  return (
+                    <TodoListItem
+                      key={item.id}
+                      index={index}
+                      item={item}
+                      updateListItem={updateListItem}
+                      deleteListItem={deleteListItem}
+                    ></TodoListItem>
+                  );
+                })
+              ) : (
+                <div className="todo-empty">
+                  <h2>Hooray!</h2>
+                  <p>Nothing left to complete.</p>
+                  <p>
+                    To add a task click on <b>Add New Item</b>
+                  </p>
+                </div>
+              )}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </DragDropContext>
       <div className="todo-item-adder" onClick={addListItem}>
         <div className="todo-add-icon">+</div>
         <div className="todo-add-text">Add new item</div>
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,31 +1,59 @@
-import './TodoList.css'
+import "./TodoList.css";
 
-const TodoListItem = ({ item, updateListItem, deleteListItem }) => {
-    const handleDone = () => {
-        item.done = !item.done;
-        updateListItem(item);
-    }
+import { Draggable } from "react-beautiful-dnd";
 
-    const handleChange = (value) => {
-        item.description = value;
-        updateListItem(item);
-    }
+const TodoListItem = ({ item, index, updateListItem, deleteListItem }) => {
+  const handleDone = () => {
+    item.done = !item.done;
+    updateListItem(item);
+  };
 
-    const handleDelete = () => deleteListItem(item);
+  const handleChange = (value) => {
+    item.description = value;
+    updateListItem(item);
+  };
 
-    return (
-        <div className="todo-list-item">
-            <div className="todo-item-checkbox">
-                <input type="checkbox" disabled={item.description ? false : true} defaultChecked={item.done} onChange={handleDone} />
-            </div>
-            <div className="todo-item-description">
-                <input type="text" className={item.done ? 'done-strike' : ''} defaultValue={item.description} placeholder="Enter description" onChange={(e) => handleChange(e.target.value)} />
-            </div>
-            <div className="todo-item-actions">
-                <i className="todo-action-button material-symbols-outlined" title="Delete" onClick={handleDelete}>delete</i>
-            </div>
+  const handleDelete = () => deleteListItem(item);
+
+  return (
+    <Draggable key={item.id} draggableId={item.id} index={index}>
+      {(provided, snapshot) => (
+        <div
+          className={`todo-list-item ${snapshot.isDragging ? "dragging" : ""}`}
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+        >
+          <div className="todo-drag-handle" {...provided.dragHandleProps}>
+            <i className="material-symbols-outlined" title="Drag">
+              drag_indicator
+            </i>
+          </div>
+          <div className="todo-item-checkbox">
+            <input
+              type="checkbox"
+              disabled={item.description ? false : true}
+              defaultChecked={item.done}
+              onChange={handleDone}
+            />
+          </div>
+          <div className="todo-item-description">
+            <input
+              type="text"
+              className={item.done ? "done-strike" : ""}
+              defaultValue={item.description}
+              placeholder="Enter description"
+              onChange={(e) => handleChange(e.target.value)}
+            />
+          </div>
+          <div className="todo-item-actions">
+            <i className="todo-action-button material-symbols-outlined" title="Delete" onClick={handleDelete}>
+              delete
+            </i>
+          </div>
         </div>
-    );
+      )}
+    </Draggable>
+  );
 };
 
 export default TodoListItem;
